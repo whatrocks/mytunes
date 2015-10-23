9ed0585a39834e07dd7f7c6f0727977b3d270c35
whatrocks <charles.harrington@gmail.com>
2015-10-22 17:35:46 -0700

    Updated styles

--- client/data/data.js
+++ client/data/data.js
@@ -1,8 +1,8 @@
 // data.js - Defines an array of data regarding song files and their accompanying details.
 var songData = [
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
-    title: "One In A Million",
+    url: "https://api.soundcloud.com/tracks/13158665",
+    title: "Sound cloud??",
     artist: "Aaliyah",
   },
   {
